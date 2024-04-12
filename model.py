ee54457923ca51ce17a6ebc7f509b394f24fa985
DinoHan <hanzeyu89@gmail.com>
2024-04-12 17:24:07 -0400

    submission

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, label_binarize
 from sklearn.linear_model import LogisticRegression
